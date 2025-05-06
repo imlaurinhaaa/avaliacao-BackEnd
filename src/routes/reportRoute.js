c4736225f1f77fb2afd6cc9fedfb3089034284fa
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-05-06 15:00:44 -0300

    feat: add apiKey report

--- src/routes/reportRoute.js
+++ src/routes/reportRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const reportController = require("./../controllers/reportController");
+const apiKeyMiddleware = require("../config/apiKey");
 
+router.use(apiKeyMiddleware);
 router.get("/report/projetos/pdf", reportController.exportProjetosPDF);
 router.get("/report/tarefas/pdf", reportController.exportTarefasPDF);
 
